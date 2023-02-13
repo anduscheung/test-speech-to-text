a96d6e520c644fa0173395f9009a4cf8528db8a5
Andus <anduscheung18@gmail.com>
2023-02-13 10:25:19 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import "./App.css";
+
+import { useState } from "react";
+import SpeechToText from "./SpeechToText";
+
+const languages = ["zh-HK", "en-US", "fr-FR", "de-DE", "it-IT"];
+
+function App() {
+  const [speechLanguage, setSpeechLanguage] = useState(languages[0]);
+  const [displayText, setDisplayText] = useState("");
+  return (
+    <div>
+      <h1>Speech sample app</h1>
+      <div style={{ marginBottom: 20 }}>
+        <label>Speech language</label>
+        <select
+          id="speechLanguage"
+          name="speechLanguage"
+          value={speechLanguage}
+          onChange={(e) => {
+            setSpeechLanguage(e.target.value);
+          }}
+        >
+          {languages.map((lang) => {
+            return (
+              <option key={lang} value={lang}>
+                {lang}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      {languages.map((lang) => {
+        return speechLanguage === lang ? (
+          <SpeechToText
+            key={lang}
+            lang={lang}
+            currentLanguage={speechLanguage}
+            setDisplayText={setDisplayText}
+          />
+        ) : null;
+      })}
+      <textarea
+        name="displayText"
+        rows={4}
+        cols={40}
+        value={displayText}
+        onChange={(e) => setDisplayText(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SpeechToText.jsx
@@ -0,0 +1,95 @@
+import { useState, useEffect, useRef } from "react";
+const speechsdk = require("microsoft-cognitiveservices-speech-sdk");
+
+const speechConfig = speechsdk.SpeechConfig.fromSubscription(
+  process.env.REACT_APP_SPEECH_KEY,
+  process.env.REACT_APP_SPEECH_REGION
+);
+
+const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+
+function SpeechToText({ lang, setDisplayText }) {
+  const recognizer = useRef(
+    new speechsdk.SpeechRecognizer(speechConfig, audioConfig)
+  );
+  const [guessText, setGuessText] = useState("");
+
+  const [recognizedText, setRecognized] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+
+  useEffect(() => {
+    setDisplayText((prev) => `${prev} ${recognizedText}`.trim());
+  }, [recognizedText, setDisplayText]);
+
+  speechConfig.speechRecognitionLanguage = lang;
+
+  const sttFromMicContinuous = () => {
+    recognizer.current.startContinuousRecognitionAsync();
+  };
+  const endMicSession = () => {
+    console.log("end mic session");
+    recognizer.current.stopContinuousRecognitionAsync(() =>
+      console.log("---->>>> stopped")
+    );
+    console.log("test end");
+  };
+
+  recognizer.current.recognizing = (s, e) => {
+    console.log(`RECOGNIZING: Text=${e.result.text}`);
+    setGuessText(e.result.text);
+  };
+
+  recognizer.current.recognized = (s, e) => {
+    if (e.result.reason === speechsdk.ResultReason.RecognizedSpeech) {
+      console.log(`RECOGNIZED: Text=${e.result.text}`);
+      setRecognized(e.result.text);
+    } else if (e.result.reason === speechsdk.ResultReason.NoMatch) {
+      console.log("NOMATCH: Speech could not be recognized.");
+    }
+  };
+
+  recognizer.current.canceled = (s, e) => {
+    console.log(`CANCELED: Reason=${e.reason}`);
+
+    if (e.reason === speechsdk.CancellationReason.Error) {
+      console.log(`"CANCELED: ErrorCode=${e.errorCode}`);
+      console.log(`"CANCELED: ErrorDetails=${e.errorDetails}`);
+    }
+
+    recognizer.current.stopContinuousRecognitionAsync();
+  };
+
+  return (
+    <div>
+      {isRecording ? "Recording..." : 'Click "Start recording" to start'}
+      <div>
+        <div style={{ display: "flex" }}>
+          <div>
+            <button
+              onClick={() => {
+                sttFromMicContinuous();
+                setIsRecording(true);
+              }}
+            >
+              Start recording
+            </button>
+            <button
+              onClick={() => {
+                endMicSession();
+                setIsRecording(false);
+              }}
+            >
+              Stop recording
+            </button>
+          </div>
+        </div>
+        <div>
+          <div>recognizing: {guessText}</div>
+          <div>final result: {recognizedText}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SpeechToText;
